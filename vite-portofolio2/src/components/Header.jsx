8b3a10fe7731851f40b7ab777b40be1f8a6c2b03
AndreasTopuh <andreasjeno23@gmail.com>
2023-11-08 06:19:16 +0800

    update

---
+++ vite-portofolio2/src/components/Header.jsx
@@ -0,0 +1,28 @@
+function Header() {
+  return (
+    <header className="d-print-none">
+      <div className="container text-center text-lg-left">
+        <div className="py-3 clearfix">
+          <h1 className="site-title mb-0">Andreas Jeno Figo Topuh</h1>
+          <div className="site-nav">
+            <nav role="navigation">
+              <ul className="nav justify-content-center">
+                <li className="nav-item"><a className="nav-link" href="https://th.bing.com/th/id/R.10c630532a95cf4ac1e1c765cbd679da?rik=ewX8SxFG%2bFpB2w&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2011%2f01%2fround-linkedin-icon_175142.png&ehk=n2Scc9zfzmujYWFlUhR2g%2f3Oju7wbtrJ9CClHyK8v6Y%3d&risl=&pid=ImgRaw&r=0" title="Twitter"><i className="fab-linkedin" /><span className="menu-title sr-only">Twitter</span></a>
+                </li>
+                <li className="nav-item"><a className="nav-link" href="https://www.facebook.com/templateflip" title="Facebook"><i className="fab fa-facebook" /><span className="menu-title sr-only">Facebook</span></a>
+                </li>
+                <li className="nav-item"><a className="nav-link" href="https://www.instagram.com/templateflip" title="Instagram"><i className="fab fa-instagram" /><span className="menu-title sr-only">Instagram</span></a>
+                </li>
+                <li className="nav-item"><a className="nav-link" href="https://github.com/templateflip" title="Github"><i className="fab fa-github" /><span className="menu-title sr-only">Github</span></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+
+  )
+}
+
+export default Header
