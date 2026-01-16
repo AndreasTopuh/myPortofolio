acb99e2a4dfbff5bc37b709c1063924fc3c18e0a
AndreasTopuh <andreasjeno23@gmail.com>
2026-01-16 13:30:59 +0800

    Remove emailjs config from tracking and fix gitignore path

--- src/config/emailjs.js
+++
@@ -1,9 +0,0 @@
-
-
-// EmailJS Configuration
-// IMPORTANT: Do not commit this file to GitHub!
-export const EMAILJS_CONFIG = {
-    publicKey: "Zc0_pJAPaE3chR_mg",
-    serviceId: "service_gtqjb2j",
-    templateId: "template_33iv2eq"
-};
